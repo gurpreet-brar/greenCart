9078e0d4bd2a9f597f43a6f704ce2b62bfe18d4d
Gurpreet Brar <guribrar200@gmail.com>
2025-04-25 21:42:47 -0400

    add categories section in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Categories from "../components/Categories";
 import MainBanner from "../components/MainBanner";
 
 function Home() {
   return (
     <div className="mt-10">
       <MainBanner />
+      <Categories />
     </div>
   );
 }
