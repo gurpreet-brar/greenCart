c948d0f1b34d01d2d71ae9d31b6a8bc65c899d44
Gurpreet Brar <guribrar200@gmail.com>
2025-04-25 20:58:56 -0400

    add banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import MainBanner from "./components/MainBanner";
+import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const isSellerPath = useLocation().pathname.includes("seller");
+
   return (
     <div>
-      <NavBar />
-      <div>
+      {isSellerPath ? <></> : <NavBar />}
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+      >
         <Routes>
-          <Route path="/" element={<MainBanner />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </div>
