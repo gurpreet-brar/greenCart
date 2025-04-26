b0388eaec3fe619c4193445758ddb87e1e84ddbe
Gurpreet Brar <guribrar200@gmail.com>
2025-04-26 18:33:42 -0400

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   );
 }
