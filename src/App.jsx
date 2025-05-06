31f0e8afaf390db94da5b6cd76eb5246bc0e9229
Gurpreet Brar <guribrar200@gmail.com>
2025-05-06 16:36:53 -0400

    create address page to add new address

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AllProducts from "./pages/AllProducts";
 import ProductCategory from "./pages/ProductCategory";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
+import Address from "./pages/Address";
 
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -29,6 +30,7 @@ function App() {
           <Route path="/products/:category" element={<ProductCategory />} />
           <Route path="/products/:category/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/address/add" element={<Address />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
